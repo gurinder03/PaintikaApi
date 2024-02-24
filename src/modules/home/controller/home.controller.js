a13440243d532bc9f83ba08de5d241b085054cc4
vikas99144 <vk40010@gmail.com>
2024-02-24 18:11:42 +0530

    add

--- src/modules/home/controller/home.controller.js
+++ src/modules/home/controller/home.controller.js
@@ -36,7 +36,7 @@ exports.list = (payload) => {
             }
            
             if (filter) {
-                obj["$or"].push({ 'artist.name': `/${payload.filter}/i` });
+                obj["$or"].push({ 'artist.name': payload.filter });
             }
             console.log("== filter ===",filter);
             console.log("== char char ====",obj);
