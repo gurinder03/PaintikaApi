87cb87f657c3d5157a63374b85fdb179998158ef
vikas99144 <vk40010@gmail.com>
2024-05-20 13:13:36 +0530

    add

--- src/helper/mobileOTP.js
+++ src/helper/mobileOTP.js
@@ -9,11 +9,16 @@ module.exports.sendOtpSMS = async(data,msg) =>{
     let authToken = config.TWILLIO_AUTHTOKEN;
     let client = new twilio(accountSid, authToken);
     try{
+        console.log("accountSid", accountSid);
+        console.log("authToken", authToken);
       return await client.messages.create({
+            body: msg,
             from: from,
-            to: "+91" + data.email_or_mobile_number,
-            body: msg
-        });
+            to: "+91 " + data.email_or_mobile_number
+            
+        }).then((message) =>{
+            console.log(message.sid);
+        }).done()
     }catch(err){
         console.log("Twilio error ",err);
         return err;
