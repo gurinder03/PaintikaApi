8b8db526eab1698c76d6223f9c9eea903da29a8b
vikas99144 <vk40010@gmail.com>
2024-07-21 19:42:30 +0530

    updated

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,28 +1,28 @@
 
- 'use strict';
+'use strict';
 
 const mongoose  = require('mongoose');
-// console.log("== dbPath====",env.MONGO_URL);
-// const dbPath = `mongodb://${env.MONGO_URL}/${env.DB_NAME}`;
+console.log("== dbPath====",process.env.MONGO_URL);
+const dbPath = `mongodb://${process.env.MONGO_URL}/${process.env.DB_NAME}`;
 
-// // const dbPath = `mongodb://0.0.0.0:27017/paintika`;
-// mongoose.connect(dbPath, {
-//   useNewUrlParser: true
-// });
-// const db = mongoose.connection;
-// db.on("error", () => {
-//   console.log("> error occurred from the database");
-// });
-// db.once("open", () => {
-//   console.log("> successfully database connected");
-// });
+// const dbPath = `mongodb://0.0.0.0:27017/paintika`;
+mongoose.connect(dbPath, {
+  useNewUrlParser: true
+});
+const db = mongoose.connection;
+db.on("error", () => {
+  console.log("> error occurred from the database");
+});
+db.once("open", () => {
+  console.log("> successfully database connected");
+});
 
-// process.on('SIGINT', function () {
-//   mongoose.connection.close(function () {
-//     console.log('Mongoose disconnected ');
-//     process.exit(0);
-//   });
-// });
+process.on('SIGINT', function () {
+  mongoose.connection.close(function () {
+    console.log('Mongoose disconnected ');
+    process.exit(0);
+  });
+});
 
- module.exports =  mongoose;
+module.exports =  mongoose;
 
