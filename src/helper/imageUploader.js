c21f15fad232e6766b9daf2b0835eca10da00605
vikas99144 <vk40010@gmail.com>
2024-07-21 20:44:50 +0530

    ad

--- src/helper/imageUploader.js
+++ src/helper/imageUploader.js
@@ -14,7 +14,7 @@ const s3 = new S3Client({
 
 const s3Storage = multerS3({
   s3: s3, 
-  bucket: process.env.AWS_BUCKET,
+  bucket: "paintika",
   metadata: (req, file, cb) => {
       cb(null, {fieldname: file.fieldname})
   },
